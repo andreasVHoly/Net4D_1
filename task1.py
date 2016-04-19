1ef99ef5c26809b3cf9eee21da11d9000a40d531
Andreas <andreas409@gmail.com>
2016-04-19 10:40:50 +0200

    more splitting done

--- task1.py
+++ task1.py
@@ -6,10 +6,23 @@
 import subprocess as sp
 
 
-#p = sp.Popen(('tcpdump', '-l','-nn','-r', 'eth1_eth2_20110207003505', 'icmp'), stdout=sp.PIPE)#gives full line
-p = sp.Popen(('tcpdump', '-l','-nn', '-r', 'eth1_eth2_20110207003505', 'icmp', '|', 'cut', '-f', '3', '-d', '" "', '|', 'head'), stdout=sp.PIPE)#cuts
+p = sp.Popen(('tcpdump', '-l','-nn','-r', 'eth1_eth2_20110207003505', 'icmp'), stdout=sp.PIPE)#gives full line
+#p = sp.Popen(('tcpdump', '-l','-nn', '-r', 'eth1_eth2_20110207003505', 'icmp', '|', 'cut', '-f', '3', '-d', '" "', '|', 'head'), stdout=sp.PIPE)#cuts
+i = 0
+dicti = {"":0}
 for row in iter(p.stdout.readline, b''):
-    print row.rstrip()
+    #print row.rstrip()
+    splitted = row.split()
+    message = splitted[5]
+    for k in range(6,len(splitted)-2):
+        message = message + " " + splitted[k]
+    message = message[0:-1]
+    #print message
+    if message in dicti:
+        dicti[message] =  dicti[message] + 1
+    else:
+        dicti[message] = 1
+    #mylist[i] =
     #what we need to do:
         #from position 6 until we hit a , is what we are looking for
         #take output and split it into this part
@@ -17,4 +30,7 @@ for row in iter(p.stdout.readline, b''):
             #then use relevant parts
         #create dictionary with parts incrementing count when reencountering
             #gives us the count of each thing
-            #can be generalized more from there
+            #can be generalized more from ther
+print(dicti)
+#for item in dicti:
+    #print i

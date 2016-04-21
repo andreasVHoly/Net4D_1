35576e70d6c57b550e4fae016f58a966f951462d
Andreas <andreas409@gmail.com>
2016-04-21 22:21:12 +0200

    task4 done, gitting for testing now

--- task2.py
+++ task2.py
@@ -3,41 +3,18 @@
 #Bandwidth usage
 
 import subprocess as sp
-import os
 import task1v2 as t1
 import datetime
-import operator
-
-day1Dict = {"",0}
-day2Dict = {"",0}
-day3Dict = {"",0}
-day4Dict = {"",0}
-day5Dict = {"",0}
-day6Dict = {"",0}
-day7Dict = {"",0}
-
-
-outgoing = {"":0}
-incoming = {"":0}
 
 
 
-def decodeName(filename):
-    buf = 8
-    year = filename[10+buf:14+buf]
-    month = filename[14+buf:16+buf]
-    day = filename[16+buf:18+buf]
-    hour = filename[18+buf:20+buf]
-    minute = filename[20+buf:22+buf]
-    second = filename[22+buf:24+buf]
-    print year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second
+def readInPackets(filename, outputfilename):
+    outgoing = {"": 0}
+    incoming = {"": 0}
 
 
-sourceDest = []
-#$ ipsumdump -s -d -r traffic/eth1_eth2_20110207201002
-
-def readInPackets(filename):
-    p = sp.Popen(('ipsumdump', '-t', '-s', '-d', '--wire-length', '-r', "traffic/eth1_eth2_20110207201002"), stdout=sp.PIPE)
+    print "reading in " + filename
+    p = sp.Popen(('ipsumdump', '-t', '-s', '-d', '--wire-length', '-r', filename), stdout=sp.PIPE)
     for row in iter(p.stdout.readline, b''):
         splitter = row.split()
 
@@ -46,10 +23,12 @@ def readInPackets(filename):
         source = splitter[1]
         dest = splitter[2]
         # check for local traffic
+
         if (t1.isLocal(source) and not t1.isLocal(dest)) or (not t1.isLocal(source) and t1.isLocal(dest)):
 
             #format time
-            time = datetime.datetime.fromtimestamp(float(splitter[0])).strftime('%Y-%m-%d %H:%M:%S')
+            #time = datetime.datetime.fromtimestamp(float(splitter[0])).strftime('%Y-%m-%d %H:%M:%S') #for seconds
+            time = datetime.datetime.fromtimestamp(float(splitter[0])).strftime('%Y-%m-%d %H:%M')
 
 
             #print "time " + str(time)
@@ -70,94 +49,28 @@ def readInPackets(filename):
                 #means incoming
                 if str(time) in incoming:
                     incoming[str(time)] += int(splitter[3])
+    # delete init
+    del incoming[""]
+    del outgoing[""]
+
+    f = open(outputfilename+filename[-14:], "w")
+    #sort times
+    sortedKeys = incoming.keys()
+    sortedKeys.sort()
+    for i in sortedKeys:
+        print str(i) + "\t incoming: " + str(incoming[str(i)]) + " \tbps\t outgoing: " + str(outgoing[str(i)]) + " \tbps"
+        f.write(str(i) + "\t incoming: " + str(incoming[str(i)]) + " \tbps\t outgoing: " + str(outgoing[str(i)]) + " \tbps\n")
 
 
 
 
-
-
-
-#delete inits
-# del day1Dict[""]
-# del day2Dict[""]
-# del day3Dict[""]
-# del day4Dict[""]
-# del day5Dict[""]
-# del day6Dict[""]
-# del day7Dict[""]
 
 def main():
     filedirs = t1.readDir()
     ipsumdumpappr = True
     tcpstatappr = False
     for f in filedirs:
-        if tcpstatappr:
-            decodeName(f)
-            getBits(f)
-            getBytes(f)
-            break
-        elif ipsumdumpappr:
-            readInPackets(f)
-            break
-
-    sortedKeys = incoming.keys()
-    sortedKeys.sort()
-    #sortedincoming = sorted(incoming.keys(), key=operator.itemgetter(1))
-    for i in sortedKeys:
-         print str(i) + "\t incoming: " + str(incoming[str(i)]) + "bps\t outgoing: " + str(outgoing[str(i)]) + "bps"
-
-
-
-
-
-
-
-
-
-def getBytes(filename):
-    print "********************bytes**********************"
-    p = sp.Popen(('tcpstat', '-o', '%B', '-r', filename), stdout=sp.PIPE)
-    for row in iter(p.stdout.readline, b''):
-        splitd = row.split(".")
-        total = 0
-        for i in splitd:
-            total += int(i)
-        print "::::per sec::::"
-        print "total bps: " + str(total)
-        print "kbps: " + str(total / 1024)
-        print "mbps: " + str(total / 1024 / 1024)
-        print "::::per min::::"
-        print "bpm: " + str(total * 60)
-        print "kbpm: " + str(total * 60 / 1024)
-        print "mbpm: " + str(total * 60 / 1024 / 1024)
-        print "::::per hr ::::"
-        print "bph: " + str(total * 3600)
-        print "kbph: " + str(total * 3600 / 1024)
-        print "mbph: " + str(total * 3600 / 1024 / 1024)
-
-
-
-def getBits(filename):
-    print "********************bits**********************"
-    p = sp.Popen(('tcpstat', '-o', '%b', '-r', filename), stdout=sp.PIPE)
-    for row in iter(p.stdout.readline, b''):
-        splitd = row.split(".")
-        total = 0
-        for i in splitd:
-            total += int(i)
-        print "::::per sec::::"
-        print "total bps: " + str(total)
-        print "kbps: " + str(total * 1024)
-        print "mbps: " + str(total * 1024 / 1024)
-        print "::::per min::::"
-        print "bpm: " + str(total * 60)
-        print "kbpm: " + str(total * 60 / 1024)
-        print "mbpm: " + str(total * 60 / 1024 / 1024)
-        print "::::per hr ::::"
-        print "bph: " + str(total * 3600)
-        print "kbph: " + str(total * 3600 / 1024)
-        print "mbph: " + str(total * 3600 / 1024 / 1024)
-
+        readInPackets(f,"timefiles/timefile-")
 
 
 if __name__ == '__main__':
